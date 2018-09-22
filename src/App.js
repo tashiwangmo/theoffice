392abbd264c1f3a815b6ac2237781392f88b13e6
tashiwangmo <tashijunk@gmail.com>
2018-09-22 13:22:50 -0500

    final with readme and some CSS changes

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
     const newScore = this.state.currentScore + 1;
     this.setState({
       currentScore: newScore,
-      rightWrong: ""
+      rightWrong: "Click on an image to begin!"
     });
     if (newScore >= this.state.topScore) {
       this.setState({ topScore: newScore });
@@ -70,7 +70,7 @@ class App extends Component {
     return (
       <Wrapper>
         <Nav
-          title="The OFFICE"
+          title="the office"
           score={this.state.currentScore}
           topScore={this.state.topScore}
           rightWrong={this.state.rightWrong}
